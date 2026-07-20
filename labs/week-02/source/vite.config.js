00ff7badf0ef42341d1b31961c18b9c34d0cb000
Kitsanapong-F <09chaisu@gmail.com>
2026-07-20 16:34:06 +0700

    fix(week-02): adjust absolute base path to inclusion of publish subfolder

--- labs/week-02/source/vite.config.js
+++ labs/week-02/source/vite.config.js
@@ -1,7 +1,10 @@
 import { defineConfig } from "vite";
 
+const repositoryName = "engse203-student-labs-68543210001";
+
 export default defineConfig({
-  base: "./", 
+  // เติม publish/ เข้าไปที่ท้ายสุด เพื่อให้ตรงกับโครงสร้างปลายทางที่สคริปต์ของวิชาย้ายไฟล์ไปวางไว้
+  base: `/${repositoryName}/labs/week-02/publish/`,
   build: {
     outDir: "docs",
     emptyOutDir: true,
