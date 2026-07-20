f17691d98b2551452ccbd3613fcc75645d78d909
Kitsanapong-F <09chaisu@gmail.com>
2026-07-20 16:30:56 +0700

    fix(week-02): project-wide transition to relative pathing for deployment asset stability

--- labs/week-02/source/vite.config.js
+++ labs/week-02/source/vite.config.js
@@ -1,11 +1,7 @@
-JavaScript
 import { defineConfig } from "vite";
 
-const repositoryName = "engse203-student-labs-68543210001";
-
 export default defineConfig({
-  // ต้องตรวจสอบให้มั่นใจว่าต่อ Path ไปถึงโฟลเดอร์แล็บสัปดาห์นั้น ๆ เสมอ
-  base: `/${repositoryName}/labs/week-02/`,
+  base: "./", 
   build: {
     outDir: "docs",
     emptyOutDir: true,
